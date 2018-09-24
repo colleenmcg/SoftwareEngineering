3b35e4a8032c72063154f3c3cd7d3f4987a4b7e1
colleenmcg <35880809+colleenmcg@users.noreply.github.com>
2018-09-24 22:50:39 +0100

    Add files via upload

--- test_LCA.py
+++ test_LCA.py
@@ -1,8 +1,11 @@
 import LCA
 
-def test_LCA_1() :
+def test_LCA_Initial() :
     assert  LCA.findLCA(LCA.root,2,3) == 1
     assert  LCA.findLCA(LCA.root,4,5) == 2
     assert  LCA.findLCA(LCA.root,4,6) == 1
     assert  LCA.findLCA(LCA.root,3,4) == 1
     assert  LCA.findLCA(LCA.root,2,4) == 2
+
+def test_LCA_Root() :
+    assert LCA.findLCA(LCA.root,-1,4) == -1
