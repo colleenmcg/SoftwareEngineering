a3b971d4553a2e31822d20b11466e0c45c8fe272
colleenmcg <35880809+colleenmcg@users.noreply.github.com>
2018-09-29 19:08:13 +0100

    Comments added and test code altered

--- test_LCA.py
+++ test_LCA.py
@@ -4,50 +4,97 @@ from LCA import Node
 
 
 def test_LCA_initialize() :
+
+# testing initilize function by
+# inserting Node with val 1 into tree.
+
     x = Node(1);
     assert x.key == 1
 
-def test_LCA_no_values_init() :
+def test_LCA_no_values() :
+
+# if a function is called with no parameters
+# we must output an exception.
+
     with pytest.raises(Exception) as e_info:
         y = Node()
 
-def test_LCA_no_values_find_path() :
     with pytest.raises(Exception) as e_info:
         y = findPath()
 
-def test_LCA_no_values_find_LCA() :
     with pytest.raises(Exception) as e_info:
         y = findLCA()
 
 def test_LCA_find_path() :
-    path = [5,2,3]
+
+    path = []
+
+# Function should output a False statement if
+# we dont input a root Node.
+
     root = Node(None)
     assert LCA.findPath(root,path,2) == False
 
-    root = Node(5)
-    assert LCA.findPath(root,path,5) == True
+# Function outputs the path if we have
+# a one node tree that matches the key we are looking for.
 
-    path = []
+    root = Node(5)
+    assert LCA.findPath(root,path,5) == [5]
+
+# Function outputs the path that it has found from root to the
+# key we are looking for.
+
+    path=[]
     root.left = Node(2)
     root.right = Node(3)
     assert LCA.findPath(root,path,3) == [5,3]
 
-    root.left = None
+    path=[]
+    root.right.right = Node(7)
+    assert LCA.findPath(root,path,7) == [5,3,7]
+
+# Function outputs a False statement if we are looking
+# for a key that is not in the tree.
+
+
     assert LCA.findPath(root,path,9) == False
 
-    root.right = None
+    assert LCA.findPath(root,path,-7) == False
+
+
+    root.right.right = None
+
     assert LCA.findPath(root,path,7) == False
 
-    root.left = Node(2)
-    root.right = Node(3)
+
+    root.left = Node(6)
+    root.right = Node(7)
     assert LCA.findPath(root,path,0) == False
 
 
+
 def test_LCA_find_LCA() :
-    root = Node(7)
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+# Function outputs -1 if we try to find the LCA of
+# 2 nodes and one or both nodes are not in the tree.
+
     assert LCA.findLCA(root, 11, 17) == -1
-    assert  LCA.findLCA(LCA.root,2,3) == 1
-    assert  LCA.findLCA(LCA.root,4,5) == 2
-    assert  LCA.findLCA(LCA.root,4,6) == 1
-    assert  LCA.findLCA(LCA.root,3,4) == 1
-    assert  LCA.findLCA(LCA.root,2,4) == 2
+
+# Function outputs the LCA of 2 nodes present in the tree.
+    assert  LCA.findLCA(root,2,3) == 1
+
+    assert  LCA.findLCA(root,4,5) == 2
+
+    assert  LCA.findLCA(root,4,6) == 1
+
+    assert  LCA.findLCA(root,3,4) == 1
+
+    assert  LCA.findLCA(root,2,4) == 2
