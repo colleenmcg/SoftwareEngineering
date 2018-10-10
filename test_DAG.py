737cf3668bf8b07cc6c5c98f0706cbf6bbb37535
colleenmcg <35880809+colleenmcg@users.noreply.github.com>
2018-10-10 23:30:01 +0100

    Starting Code

---
+++ test_DAG.py
@@ -0,0 +1,19 @@
+import DAG
+import pytest
+
+def test_DAG_initialize() :
+
+# testing initilize function
+
+
+def test_DAG_add_node() :
+
+# testing add_node function
+
+def test_DAG_add_edge() :
+
+# testing add_edge function
+
+def test_DAG_LCA() :
+
+# testing LCA function
