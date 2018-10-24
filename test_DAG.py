7b549552fe564adf3af32fce252f93d26eb3f025
colleenmcg <35880809+colleenmcg@users.noreply.github.com>
2018-10-24 13:45:18 +0100

    Final Upload

--- test_DAG.py
+++ test_DAG.py
@@ -1,4 +1,4 @@
-
+#Colleen McGloin SN: 16325155
 from DAG import GRAPH
 import pytest
 
@@ -6,9 +6,6 @@ import pytest
 Graph = None
 Graph=GRAPH()
 
-#def test_DAG_initialize() :
-
-# testing initilize function
 
 def test_DAG_add_node() :
 
@@ -26,9 +23,9 @@ def test_DAG_add_edge() :
 
 # testing add_edge function
 
-    Graph.add_edge('A', 'B')
-    Graph.add_edge('A', 'C')
-    assert (Graph.graph == {'A': ['B','C'],'B': [],'C': []})
+    Graph.add_edge('A', 'B') #adding edge
+    Graph.add_edge('A', 'C') #adding edge
+    assert (Graph.graph == {'A': ['B','C'],'B': [],'C': []})  # test passes if graph has all edges added
 
 
     assert Graph.add_edge('A', 'D') == False # function should return false if destination node is not present in graph.
@@ -39,6 +36,8 @@ def test_DAG_add_edge() :
 
 
 def test_DAG_BFS() :
+    
+# testing bfs function
     
     Graph.add_node('D') #adding node
     Graph.add_node('E') #adding node
@@ -59,26 +58,31 @@ def test_DAG_BFS() :
 
     
   
-    assert  list(Graph.bfs('A', 'C')) == [['A','C']]
-    assert  list(Graph.bfs('D', 'H')) == [['D','H'],['D','E','F','H']]
-    assert  list(Graph.bfs('A', 'H')) == [['A','B','H']]
-
-
+    assert  list(Graph.bfs('A', 'C')) == [['A','C']] # function should return a list made up of path(s) from A node to C node
+    assert  list(Graph.bfs('D', 'H')) == [['D','H'],['D','E','F','H']] #  function should return a list made up of paths from D node to H node
+    assert  list(Graph.bfs('A', 'H')) == [['A','B','H']] #  function should return a list made up of path(s) from A node to H node
 
 
 def test_to_Flat_List() :
-    temp = list(Graph.bfs('D', 'H'))
-    assert list(Graph.toFlatList(temp)) == ['D','H','D','E','F','H']
+    
+# testing to_Flat_List function
+
+    temp = list(Graph.bfs('D', 'H')) # getting list made up of multiple paths from D to H and assigning list to temp
+    assert list(Graph.toFlatList(temp)) == ['D','H','D','E','F','H'] # this function converts a list that contains multiple lists into just one list
 
 def test_common_Ancestor() :
     
-    assert list(Graph.commonAncestor([['A','E'],['A','B','E','F']],[['A','C','F']])) == ['F']
+# testing common_Ancestor function
+    
+    assert list(Graph.commonAncestor([['A','E'],['A','B','E','F']],[['A','C','F']])) == ['F'] #this function returns 'F' which is the Lowest Common Ancestor of the 2 paths i inputted into the function
 
 
 def test_LCA() :
+    
+# testing LCA function
 
-    assert list(Graph.LCA('D','F','G')) == ['E']
-    assert list(Graph.LCA('A','H','I')) == ['A']
-    assert list(Graph.LCA('D','H','I')) == ['E']
+    assert list(Graph.LCA('D','F','G')) == ['E'] # this function returns 'E' which is the LCA of the 2 nodes F and G from the root D
+    assert list(Graph.LCA('A','H','I')) == ['A'] # this function returns 'A' which is the LCA of the 2 nodes H and I from the root A
+    assert list(Graph.LCA('D','H','I')) == ['E'] # this function returns 'E' which is the LCA of the 2 nodes H and I from the root D
 
 
